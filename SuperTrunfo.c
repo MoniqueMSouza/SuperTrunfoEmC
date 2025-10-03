0b783c273329880dd54fac79e490054ee351e0f1
MoniqueMSouza <moniquemoreira.mkt@gmail.com>
2025-10-03 08:18:25 -0300

    Feat: Registra as informações da Carta 1

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -20,5 +20,29 @@ int main () {
     float C2pib;
     int C2pontosturisticos;
 
+    //Registrando as informações da Carta 1 
+    printf("Primeiro, vamos registrar as informações da CARTA 1. \n"); 
+
+    printf("Digite apenas uma letra: De 'A' a 'H'. \n"); 
+    scanf ("%s", &C1estado); 
+
+    printf("Digite a letra do estado seguida de um número de 01 a 04 (ex: A01, B03). \n"); 
+    scanf ("%s", &C1codigo); 
+
+    printf("Digite o nome da cidade. \n"); 
+    scanf ("%s", &C1nome); 
+
+    printf("Digite o número de Habitantes. \n"); 
+    scanf ("%d", &C1populaçao); 
+
+    printf("Digite a área da cidade em quilômetros quadrados. \n"); 
+    scanf ("%f", &C1area); 
+
+    printf("Digite o Produto Interno Bruto da cidade. \n"); 
+    scanf ("%f", &C1pib); 
+
+    printf("Digite a quantidade de pontos turísticos na cidade. \n"); 
+    scanf ("%d", &C1pontosturisticos);
+
 
 }
