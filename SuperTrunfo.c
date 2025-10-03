0d5570f04b39a3de9a7ec322729179bb786756d6
MoniqueMSouza <moniquemoreira.mkt@gmail.com>
2025-10-03 08:15:06 -0300

    Feat: Declara variáveis para armazenar informações das cartas 1 e 2

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -2,4 +2,23 @@
 
 int main () {
 
+    //Informações da Carta 1 - C1
+    char C1estado[50];
+    char C1codigo[50];
+    char C1nome[50];
+    int C1populaçao;
+    float C1area;
+    float C1pib;
+    int C1pontosturisticos;
+
+    //Informações da Carta 2 - C2
+    char C2estado[50];
+    char C2codigo[50];
+    char C2nome[50];
+    int C2populaçao;
+    float C2area;
+    float C2pib;
+    int C2pontosturisticos;
+
+
 }
